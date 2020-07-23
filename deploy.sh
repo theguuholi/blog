d8ccba412ef6be69d9bcc3477b1fad878b3f230c
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-07-23 05:22:25 -0300

    update

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
-
-echo "Push gigalixir to master banch gigalixir"
-git push gigalixir master HEAD:master
-echo "Deploy Finalizado"
-echo "Exiting."
