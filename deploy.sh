2f659d987bbddd930dc66a2932f7d9cfbd1d3922
Gustavo <g.92oliveira@gmail.com>
2020-07-23 05:31:53 -0300

    Revert "Feature/processcd"

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "====Starting Gigalixir Deploy====="
-git remote add gigalixir https://andressak.oliveira23%40gmail.com:59e86131-f8aa-4117-b47a-b39861417b3a@git.gigalixir.com/bloggustavo.git
-git push gigalixir HEAD:master
-echo "====End Gigalixir Deploy====="
-echo "Exiting."
