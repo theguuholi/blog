bc0e9dcd33f91f0a7fc6cf924e8fe038a3da30d5
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-07-31 18:58:05 -0300

    inclusao de comentarios

--- lib/blog_web/channels/comments_channel.ex
+++ lib/blog_web/channels/comments_channel.ex
@@ -1,7 +1,10 @@
 defmodule BlogWeb.CommentsChannel do
   use BlogWeb, :channel
 
-  def join(name, _payload, socket), do: {:ok, %{nome: "Deu certo!!"}, socket}
+  def join("comments:"<> post_id, payload, socket) do
+    post = Blog.Posts.get_post_with_comments!(post_id)
+    {:ok, %{comments: post.comments}, socket}
+  end
 
   def handle_in() do
 
