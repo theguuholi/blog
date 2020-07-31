9deecd94b0e34be0e5d17b4fb250a9c6d03a7dc7
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-07-31 09:04:16 -0300

    contextos

---
+++ priv/repo/migrations/20200731115546_create_comments.exs
@@ -0,0 +1,12 @@
+defmodule Blog.Repo.Migrations.CreateComments do
+  use Ecto.Migration
+
+  def change do
+    create table(:comments) do
+      add :content, :string
+
+      timestamps()
+    end
+
+  end
+end
