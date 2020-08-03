e05294a280c157550f2d834b2f771ed6d9dffc37
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-03 09:12:31 -0300

    autenticacao

--- lib/blog_web/controllers/auth_controller.ex
+++ lib/blog_web/controllers/auth_controller.ex
@@ -1,18 +1,30 @@
 defmodule BlogWeb.AuthController do
   use BlogWeb, :controller
+  alias Blog.Accounts
 
   plug Ueberauth
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do\
-    user = %{token: auth.credentials.token,
-    email: auth.info.email,
-    first_name: auth.info.first_name,
-    last_name: auth.info.last_name,
-    image: auth.info.image,
-    provider: provider
-  }
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, %{"provider" => provider}) do
+    user = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      first_name: auth.info.first_name,
+      last_name: auth.info.last_name,
+      image: auth.info.image,
+      provider: provider
+    }
 
-    IO.inspect(user)
-    render(conn, "index.html")
+    case Accounts.create_user(user) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Bem vindo!!! #{user.email}")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _erro} ->
+        conn
+        |> put_flash(:error, "Algo deu Errado")
+        |> redirect(to: Routes.page_path(conn, :index))
+    end
   end
 end
