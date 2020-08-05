93633f7679bfb5c3458943b66afa39a334efd1f4
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-05 09:16:03 -0300

    update post

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -3,6 +3,8 @@ defmodule BlogWeb.PostController do
 
   alias Blog.{Posts, Posts.Post}
 
+  plug BlogWeb.Plug.RequireAuth when action in [:create, :new, :edit, :update, :delete]
+
   def index(conn, _params) do
     posts = Posts.list_posts()
     render(conn, "index.html", posts: posts)
