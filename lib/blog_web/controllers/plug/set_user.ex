0351ed616e155e235a0f13c4b14580addf7d32fc
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-05 10:22:32 -0300

    ajustes pipeline

--- lib/blog_web/controllers/plug/set_user.ex
+++ lib/blog_web/controllers/plug/set_user.ex
@@ -9,10 +9,11 @@ defmodule BlogWeb.Plug.SetUser do
     user_id = get_session(conn, :user_id)
 
     user = user_id && Accounts.get_user!(user_id)
-    if user != nil  do
-        assign(conn, :user, user)
+
+    if user != nil do
+      assign(conn, :user, user)
     else
-        assign(conn, :user, nil)
+      assign(conn, :user, nil)
     end
   end
 end
