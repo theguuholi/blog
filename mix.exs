549e83d499fc92d8f9691e0d2ebe6b8f3df26225
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-08-06 11:04:46 -0300

    testes com usuario na secao

--- mix.exs
+++ mix.exs
@@ -88,7 +88,7 @@ defmodule Blog.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.reset --quiet", "test"]
     ]
   end
 end
